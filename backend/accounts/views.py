b6090dfe003fe70563a66ceeeb9643393c770c48
Jasur <ibraimov0409@mail.ru>
2022-08-26 10:15:13 +0600

    Backend: Created Serializer for Model and view get_profile

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Profile
+from .serializers import *
+
+@api_view(['GET'])
+def get_profile(request, id):
+    if request.method == 'GET':
+        profile = Profile.objects.get(id=id)
+        serializer = ProfileSerializer(profile, context={'request': request})
+        return Response({'data': serializer.data })
